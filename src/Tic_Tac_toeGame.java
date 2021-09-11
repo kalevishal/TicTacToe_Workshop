a60a61b81b3267505475f0e44e143351657b750e
kalevishal <kalevishal1610@gmail.com>
2021-09-11 16:34:25 +0530

    Added Player's Choice

--- src/Tic_Tac_toeGame.java
+++ src/Tic_Tac_toeGame.java
@@ -1,15 +1,35 @@
-public class Tic_Tac_ToeGame{
-	
+import java.util.Scanner;
+
+public class Tic_Tac_ToeGame {
     static char[] board = new char[10];
     public static void main(String args[]) {
         System.out.println("Welcome to Tic-Tac-Toe game");
         board();
+        PlayerOption();
     }
-
-    public static void board() {
-
+    private static void board() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
+        }
+    }
+    public static void PlayerOption() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Player should select X or O: ");
+        char playerschoice = sc.next().charAt(0);
+        char computerchoice;
+        if(playerschoice == 'O'|| playerschoice =='X')
+        {
+            System.out.println("Valid Selection");
+            if (playerschoice=='O')
+            {
+                computerchoice = 'X';
+            }
+            else
+                computerchoice='O';
+        }
+        else
+        {
+            System.out.println("Invalid Selection");
         }
     }
 }
