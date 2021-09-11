dffaa36343e9126dfcf34c544e64f2f7d9670e73
kalevishal <kalevishal1610@gmail.com>
2021-09-11 19:20:35 +0530

    Modified Method ShowBoard

--- src/Tic_Tac_toeGame.java
+++ src/Tic_Tac_toeGame.java
@@ -6,6 +6,7 @@ public class Tic_Tac_ToeGame {
         System.out.println("Welcome to Tic-Tac-Toe game");
         board();
         PlayerOption();
+        showBoard();
     }
     private static void board() {
         for (int i = 1; i < board.length; i++) {
@@ -31,5 +32,17 @@ public class Tic_Tac_ToeGame {
         {
             System.out.println("Invalid Selection");
         }
+    }
+    public static void showBoard() {
+        System.out.println(" ----------- ");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+        System.out.println(" ----------- ");
     }
 }
